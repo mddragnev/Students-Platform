d24ff44f9356d24185916ff555e9e73815516d70
Martin Dragnev <MDragnev@infragistics.com>
2020-02-10 02:21:41 +0200

    Begining of the front-end part (posts)

--- students-platform/src/app/app.component.ts
+++ students-platform/src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 
+import { Post } from './posts/post.model';
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'students-platform';
+ posts: Post[] = [];
+
+ onPostAdded(post) {
+  this.posts.push(post);
+ }
 }
