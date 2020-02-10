bbd855d8d6c472af46720a7957565ec8e19a4134
Martin Dragnev <MDragnev@infragistics.com>
2020-02-11 01:27:51 +0200

    Adding db

--- students-platform/src/app/posts/posts.service.ts
+++ students-platform/src/app/posts/posts.service.ts
@@ -1,5 +1,6 @@
 import { Post } from './post.model';
 import { Subject } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
@@ -12,9 +13,18 @@ export class PostsSerivce {
   constructor(private http: HttpClient) {}
 
   getPosts() {
-    this.http.get<{ message: string, posts: Post[] }>('http://localhost:3000/api/posts')
-      .subscribe( (postData) => {
-        this.posts = postData.posts;
+    this.http.get<{ message: string, posts: any }>('http://localhost:3000/api/posts')
+      .pipe(map(postData => {
+        return postData.posts.map(post => {
+          return {
+            title: post.title,
+            content: post.content,
+            id: post._id
+          };
+        });
+      }))
+      .subscribe( posts => {
+        this.posts = posts;
         this.postsUpdated.next([...this.posts]);
       });
   }
@@ -25,10 +35,20 @@ export class PostsSerivce {
 
   addPost(title: string, content: string) {
     const post: Post = { id: null, title, content };
-    this.http.post<{message: string}>('http://localhost:3000/api/posts', post)
+    this.http.post<{message: string, id: string}>('http://localhost:3000/api/posts', post)
       .subscribe( (responseData) => {
-        console.log(responseData.message);
+        const postID = responseData.id;
+        post.id = postID;
         this.posts.push(post);
+        this.postsUpdated.next([...this.posts]);
+      });
+  }
+
+  deletePost(id: string) {
+    this.http.delete('http://localhost:3000/api/posts/' + id)
+      .subscribe( () => {
+        const updatedPosts = this.posts.filter(post => post.id !== id);
+        this.posts = updatedPosts;
         this.postsUpdated.next([...this.posts]);
       });
   }
